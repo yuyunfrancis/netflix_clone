9e38dab2f4dacd55d494173c6ae6d37ceca19193
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-01-12 16:25:25 +0100

    netflix clone banner and row

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -24,8 +25,11 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
