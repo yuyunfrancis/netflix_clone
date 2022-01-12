3415abbde92346bc7e03d26c689f9599f47387a7
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-01-12 06:05:37 +0100

    first commit

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -14,9 +15,21 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
+  console.log(movies);
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/* container -> posters */}
     </div>
   );
--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
- import axios from "axios";
+import axios from "axios";
 
- const instance = axios.create({
-     baseURL: "https://api.themoviedb.org/3",
- })
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
 
- instance.get('/foo-bar')
- export default instance
+instance.get("/foo-bar");
+export default instance;
